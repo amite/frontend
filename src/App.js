96ef8e3edc0cfc82795a55bee250a508ed60f0f4
amite <amit.erandole@gmail.com>
2018-09-14 17:50:07 +0530

    content changed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Docker!</h1>
         </header>
-        <p className="App-intro">Hi there sfasf </p>
+        <p className="App-intro">Hi there</p>
       </div>
     );
   }
